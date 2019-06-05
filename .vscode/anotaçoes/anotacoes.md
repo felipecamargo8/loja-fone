34a32ed0becc2ff9ffb5c8f02f05ff32a855ce7d
Felipe.Camargo <flpcamargo8@gmail.com>
2019-06-04 22:58:28 -0300

    Fim aula 16

---
+++ .vscode/anotaçoes/anotacoes.md
@@ -0,0 +1,23 @@
+#JavaScript
+
+## Descripition-product. js
+
+### apenas exemplos
+
+const $heart = window.document.querySelector(".-heart");
+const $stars = document.querySelectorAll(".star");
+let index = 0;
+
+\$heart.addEventListener("click", handleClickHeart);
+
+$stars.forEach(function($star) {
+\$star.addEventListener("click", handleStar);
+});
+
+function handleStar() {
+this.classList.toggle("-active");
+}
+
+function handleClickHeart() {
+\$heart.classList.toggle("-active");
+}
--- js/descripition-product.js
+++ js/descripition-product.js
@@ -1,17 +1,50 @@
-const $heart = window.document.querySelector(".-heart");
+const $heart = document.querySelector(".-heart");
 const $stars = document.querySelectorAll(".star");
-let index = 0;
+const positionLast = $stars.length - 1;
+/* Variavel devolve o tamanho do arrail o menos um, pois inicia em 0*/
+$heart.addEventListener("click", handleClick);
 
-$heart.addEventListener("click", handleClickHeart);
+$stars.forEach(function($star, key) {
+  if (key == 0) {
+    $star.addEventListener("click", fistStar);
+  }
 
-$stars.forEach(function($star) {
-  $star.addEventListener("click", handleStar);
+  if (key == positionLast) {
+    $star.addEventListener("click", lastStar);
+  }
+
+  if (key > 0 && key < positionLast) {
+    $star.addEventListener("click", function() {
+      middleStar(key);
+    });
+  }
 });
+//esta função pega a key das estrelas  de qual foi clicada e preenche as anterior
+function middleStar(index) {
+  $stars.forEach(function($star) {
+    $star.classList.remove("-active");
+  });
 
-function handleStar() {
-  this.classList.toggle("-active");
+  $stars.forEach(function($star, key) {
+    if (key <= index) {
+      $star.classList.add("-active");
+    }
+  });
+}
+// esta função Preenche todas as estrelas e/ou remove ate a primeira estrela
+function fistStar() {
+  $stars.forEach(function($star) {
+    $star.classList.remove("-active");
+  });
+  this.classList.add("-active");
+}
+// essa função preenche as estrelas todas clicando na estrela do fim
+function lastStar() {
+  $stars.forEach(function($star) {
+    $star.classList.add("-active");
+  });
 }
 
-function handleClickHeart() {
-  $heart.classList.toggle("-active");
+function handleClick() {
+  this.classList.toggle("-active");
 }
